023b8d971d250da2de8c74949dd40569e68dc450
rs <93507446+cx0y@users.noreply.github.com>
2024-10-28 11:32:51 +0530

    Update kex.sh
    fixed, arch linux test problem

--- kex.sh
+++ kex.sh
@@ -12,7 +12,7 @@ sleep 1
 curl -O LICENSE https://raw.githubusercontent.com/LevelUpInc/kex/refs/heads/main/LICENSE
 wget -O kex_i.sh https://offs.ec/2MceZWr
 chmod +x kex_i.sh
-#./kex_i.sh
+./kex_i.sh
 sleep 10 
 rm -rf kex_i.sh
 echo "All Done"
